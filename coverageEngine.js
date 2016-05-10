5e8fc32592520fba36503bf96834170f63f6a122
unknown <lance.shi@FTR-BNE-WKS14.corp.fortherecord.com>
2016-05-10 17:35:08 +1000

    Initial commit

---
+++ coverageEngine.js
@@ -0,0 +1,526 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var Q = require('q');
+var fs = require('fs');
+var lo = require('lodash');
+var jsforce = require('jsforce');
+var csvWriter = require('csv-write-stream');
+var sleep = require('system-sleep');
+var sf_deploy_url = '';
+var sf_deploy_username = '';
+var sf_deploy_password = '';
+
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.get('/home', function(req, res) {
+  res.sendFile(path.join(__dirname + '/getCoverage.html'));
+});
+
+app.post('/home', function(req, res) {
+  sf_deploy_url = 'login.salesforce.com';
+  sf_deploy_username = req.body.sfUserName;
+  sf_deploy_password = req.body.sfPwd;
+  queueSteps(res);
+
+});
+
+app.get('/result', function(req, res) {
+  res.sendFile(path.join(__dirname + '/teststats.html'));
+});
+
+app.listen(3000);
+
+
+/** The salesforce client */
+var sfdc_client = null;
+
+/** A map of class Ids to class information */
+var id_to_class_map = {};
+
+/** A map of test class Ids to class information */
+var test_class_map = {};
+var classes_to_be_recompiled = {};
+
+/** A map of the coverage stats */
+var coverage_stats = {};
+
+/** File name to write the CSV data to */
+var CSVFilename = 'teststats.csv';
+/** File name to write the HTML data to */
+var HTMLFilename = 'teststats.html';
+
+
+/**
+* Log into the salsforce instance
+*/
+var sfdcLogin = function () {
+	'use strict';
+
+	var deferred = Q.defer();
+
+	console.log('Logging in as ' + sf_deploy_username);
+  sfdc_client = new jsforce.Connection({loginUrl : 'https://' + sf_deploy_url, version:'36.0'});
+
+	sfdc_client.login(sf_deploy_username, sf_deploy_password, function (error, res) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			console.log('Logged in');
+			deferred.resolve();
+		}
+	});
+
+	return deferred.promise;
+};
+
+/**
+* Logout of the salsforce instance
+*/
+var sfdcLogout = function () {
+	'use strict';
+
+	var deferred = Q.defer();
+	sfdc_client.logout(function (error, res) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			console.log('Logged out');
+			deferred.resolve();
+		}
+	});
+
+	return deferred.promise;
+};
+
+/**
+* Builds a map of class id to class data
+*/
+var buildClassIdToClassDataMap = function () {
+	'use strict';
+
+	var deferred = Q.defer();
+
+	console.log('Fetching class information');
+
+	sfdc_client.tooling.sobject('ApexClass').find({NamespacePrefix:'',Status:'Active'},{Id:1,Name:1,Body:1,IsValid:1}).execute(function (error, data) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			console.log('Got information about ' + lo.size(data) + ' classes');
+
+			lo.forEach(data, function (row) {
+				if (!row.IsValid) {
+					console.log('WARNING: Class ' + row.Name + ' needs to be recompiled for proper test coverage');
+					classes_to_be_recompiled[row.Name] = row.Name;
+				}
+				if (row.Body.toLowerCase().indexOf('@istest') === -1) {
+					id_to_class_map[row.Id] = {
+						name: row.Name,
+						source: row.Body,
+						coverage: []
+					};
+				} else {
+					test_class_map[row.Id] = {
+						name: row.Name,
+						source: row.Body
+					};
+				}
+			});
+
+			deferred.resolve();
+		}
+	});
+
+	return deferred.promise;
+};
+
+var buildAddTriggersToClassIDMap = function () {
+	'use strict';
+
+	var deferred = Q.defer();
+
+	console.log('Fetching trigger information');
+
+	// Get the Trigger info too
+	sfdc_client.tooling.sobject('ApexTrigger').find({NamespacePrefix:'',Status:'Active'},{Id:1,Name:1,Body:1,IsValid:1}).execute(function (error, triggerData) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			console.log('Got information about ' + lo.size(triggerData) + ' triggers');
+
+			lo.forEach(triggerData, function (row) {
+				if (!row.IsValid) {
+					console.log('WARNING: Trigger ' + row.Name + ' needs to be recompiled for proper test coverage');
+					classes_to_be_recompiled[row.Name] = row.Name;
+				}
+				if (row.Body.toLowerCase().indexOf('@istest') === -1) {
+					id_to_class_map[row.Id] = {
+						name: row.Name,
+						source: row.Body,
+						coverage: []
+					};
+				} else {
+					test_class_map[row.Id] = {
+						name: row.Name,
+						source: row.Body
+					};
+				}
+			});
+
+			deferred.resolve();
+		}
+	});
+}
+
+/**
+* Runs all tests with the tooling api
+*/
+var runAllTests = function () {
+	'use strict';
+
+	var class_ids = lo.keys(test_class_map),
+		deferred = Q.defer();
+
+	sfdc_client.tooling.runTestsAsynchronous(class_ids, function (error, data) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			deferred.resolve(data);
+		}
+	});
+
+	return deferred.promise;
+};
+
+/**
+* Query the test results
+*
+* @param testRunId The id of the test run
+* @param deferred The Q.defer instance
+*/
+var queryTestResults = function myself(testRunId, deferred) {
+	'use strict';
+
+	var isComplete = true;
+
+	var pending = 0;
+
+	sfdc_client.query('select Id, Status, ApexClassId from ApexTestQueueItem where ParentJobId = \'' + testRunId + '\'', function (error, data) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			lo.each(data.records, function (row) {
+				if (row.Status === 'Queued' || row.Status === 'Processing') {
+					isComplete = false;
+					pending++;
+				}
+			});
+
+			if (isComplete) {
+				deferred.resolve();
+			} else {
+				console.log('There are ' + pending + ' still running, sleeping for ' + pending + ' seconds before checking again.');
+				sleep(pending *1000);
+				myself(testRunId, deferred);
+			}
+		}
+	});
+};
+
+/**
+* Waits until all tests are completed
+*
+* @param testRunId The id of the test run
+*/
+var waitUntilTestsComplete = function (testRunId) {
+	'use strict';
+
+	var deferred = Q.defer();
+	console.log('Waiting for tests');
+	queryTestResults(testRunId, deferred);
+
+	return deferred.promise;
+};
+
+/**
+* Gets the test data and builds an array of the number of times the line was tested
+*/
+var buildCoverage = function () {
+	'use strict';
+
+	var max_line, coverage_size, class_id, i,
+		deferred = Q.defer();
+
+	console.log('Fetching code coverage information');
+	coverage_stats['Total Org Coverage'] = {
+		NumLinesCovered:0,
+		NumLinesUncovered:0,
+		TotalLines:0,
+		Coverage:0
+	};
+
+	sfdc_client.tooling.sobject('ApexCodeCoverageAggregate').find({}).execute(function (error, data) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			console.log('Got information about ' + lo.size(data) + ' tests');
+
+			lo.forEach(data, function (row) {
+				class_id = row.ApexClassOrTriggerId;
+
+				if (lo.has(id_to_class_map, class_id)) {
+					var class_name = lo.toString(id_to_class_map[class_id].name);
+					coverage_stats[class_name] = {
+						NumLinesCovered:row.NumLinesCovered,
+						NumLinesUncovered:row.NumLinesUncovered,
+						TotalLines:(row.NumLinesCovered + row.NumLinesUncovered),
+						Coverage:lo.round((((row.NumLinesCovered + row.NumLinesUncovered) - row.NumLinesUncovered)/(row.NumLinesCovered + row.NumLinesUncovered))*100, 2)
+					};
+					coverage_stats['Total Org Coverage'] = {
+						NumLinesCovered:(coverage_stats['Total Org Coverage'].NumLinesCovered + row.NumLinesCovered),
+						NumLinesUncovered:(coverage_stats['Total Org Coverage'].NumLinesUncovered + row.NumLinesUncovered),
+						TotalLines:(coverage_stats['Total Org Coverage'].TotalLines + row.NumLinesCovered + row.NumLinesUncovered)
+					};
+					coverage_stats['Total Org Coverage'].Coverage = lo.round(((coverage_stats['Total Org Coverage'].TotalLines - coverage_stats['Total Org Coverage'].NumLinesUncovered)/coverage_stats['Total Org Coverage'].TotalLines)*100, 2)
+					//console.log(coverage_stats['Total Org Coverage']);
+					max_line = lo.max(lo.union(row.Coverage.coveredLines, row.Coverage.uncoveredLines));
+					coverage_size = lo.size(id_to_class_map[class_id].coverage);
+
+					if (max_line > coverage_size) {
+						for (i = coverage_size; i <= max_line; i += 1) {
+							id_to_class_map[class_id].coverage.push(null);
+						}
+					}
+
+					lo.forEach(row.Coverage.coveredLines, function (line_number) {
+						if (id_to_class_map[class_id].coverage[line_number - 1] === null) {
+							id_to_class_map[class_id].coverage[line_number - 1] = 1;
+						} else {
+							id_to_class_map[class_id].coverage[line_number - 1] += 1;
+						}
+					});
+
+					lo.forEach(row.Coverage.uncoveredLines, function (line_number) {
+						if (id_to_class_map[class_id].coverage[line_number - 1] === null) {
+							id_to_class_map[class_id].coverage[line_number - 1] = 0;
+						}
+					});
+				}
+			});
+			//console.log(id_to_class_map);
+			deferred.resolve();
+		}
+	});
+
+	return deferred.promise;
+};
+
+/**
+* Process data to CSV
+*/
+var saveToCSVRows = function () {
+	'use strict';
+
+	//console.log(coverage_stats);
+
+	var writer = csvWriter(),
+		deferred = Q.defer();
+	writer.pipe(fs.createWriteStream(CSVFilename));
+	//coverage_stats.sort();
+
+	lo.forEach(coverage_stats, function (row, apexClassName) {
+
+		writer.write({"Apex Class/Trigger Name":apexClassName,"Coverage Percentage":row['Coverage']});
+
+		if (apexClassName == 'Total Org Coverage') {
+			console.log('Total Org Coverage: ' + row['Coverage'] + ' (' + row['NumLinesCovered'] + '/' + row['TotalLines'] + ')');
+		}
+	});
+	writer.end();
+	deferred.resolve();
+	return deferred.promise;
+}
+
+/**
+* Process data to CSV
+*/
+var saveToCSVCols = function () {
+	'use strict';
+
+	console.log('Writing to file for Jenkins');
+
+	var writer = csvWriter(),
+		deferred = Q.defer();
+	writer.pipe(fs.createWriteStream(CSVFilename));
+	//coverage_stats.sort();
+
+	writer.write(lo.mapValues(coverage_stats, 'Coverage'));
+
+	console.log('Total Org Coverage: ' + coverage_stats['Total Org Coverage'].Coverage + ' (' + coverage_stats['Total Org Coverage'].NumLinesCovered + '/' + coverage_stats['Total Org Coverage'].TotalLines + ')');
+	writer.end();
+	deferred.resolve();
+	return deferred.promise;
+}
+
+var writeHTML = function () {
+	'use strict';
+
+	var deferred = Q.defer();
+	var graph_height = 1000 + (lo.size(coverage_stats) * 10);
+	var html = '<head>\n  <!-- Plotly.js -->\n  <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>\n</head>\n\n<body>\n  \n  <div id="myDiv" style="width: 100%; height: '+graph_height+'px;"><!-- Plotly chart will be drawn inside this DIV --></div>\n  <script>\n    <!-- JAVASCRIPT CODE GOES HERE -->';
+	var script_end = ';\nvar layout = {\n\ttitle: \'Test Coverage\',\n\thovermode:\'closest\',\n\tbarmode:\'overlay\',\n\tmargin: {\n\t\tl:250\n\t},\tshowlegend: true,\n\tlegend: {\n\t\tx: 0,\n\t\ty: 100\n\t}\n\t};\nPlotly.newPlot(\'myDiv\', data, layout);</script>'
+	var html_end = '</body>';
+
+    var temp = lo.mapValues(coverage_stats, 'Coverage');
+    console.log(temp);
+    var keys = lo.keys(temp).sort().reverse();
+    // Remove the Total Org Coverage from the keys
+    lo.pull(keys,'Total Org Coverage');
+    // Add it back at the end so it displays at the top of the graph
+    keys.push('Total Org Coverage');
+    var keys_str = '';
+    var values_str = '';
+    var sfCover = '';
+    var orgCover = '';
+    try {
+	    lo.forEach(keys,function(key) {
+		    // console.log('key:' + keys_str.length + ', value:' + values_str.length);
+		    // console.log('sfCover:' + sfCover.length + ', orgCover:' + orgCover.length);
+	    	if (keys_str.length == 0) {
+	    		keys_str = keys_str + '\'' + key + '\'';
+	    	} else {
+	    		keys_str = keys_str + ', ' + '\'' + key + '\'';
+	    	}
+
+	    	var value = temp[key];
+	    	if (values_str.length == 0) {
+	    		values_str = values_str + value;
+	    		sfCover = sfCover + 75;
+	    		orgCover = orgCover + 85;
+	    	} else {
+	    		values_str = values_str + ', ' + value;
+	    		sfCover = sfCover + ', ' + 75;
+	    		orgCover = orgCover + ', ' + 85;
+	    	}
+	    });
+	} catch (error) {
+	    console.log('key:' + keys_str + ', values_str:' + values_str + ', sfCover:' + sfCover + ', orgCover:' + orgCover);
+
+		deferred.reject(new Error(error));
+		return deferred.promise;
+	}
+
+    var data = '\nvar trace1={\n\
+    	name:\'Code Coverage %\',\n\
+    	//hoverinfo:"x+y",\n\
+    	y:[' + keys_str + '],\n\
+    	x:[' + values_str + '],\n\
+    	type: \'bar\',\n\
+    	orientation:\'h\',\n\
+    	showlegend: false\n\
+    };\n\
+    var trace2={\n\
+    	name:\'Salesforce Required Coverage Level\',\n\
+    	//hoverinfo:\'Salesforce Required Coverage Level\',\n\
+    	y:[' + keys_str + '],\n\
+    	x:[' + sfCover + '],\n\
+    	type: \'scatter\',\n\
+		mode:\'lines\',\n\
+		showlegend: true,\n\
+		marker: {\n\
+			color: \'red\',\n\
+			width: 3\n\
+		}\n\
+    };\n\
+    var trace3={\n\
+		name:\'Org Required Coverage Level\',\n\
+    	y:[' + keys_str + '],\n\
+    	x:[' + orgCover + '],\n\
+    	type: \'scatter\',\n\
+		mode:\'lines\',\n\
+		showlegend: true,\n\
+		marker: {\n\
+			color: \'green\',\n\
+			width: 3\n\
+		}\n\
+    };\n\
+    var data=[trace1,trace2,trace3]';
+
+    var uncompiledClasses = '';
+    if (lo.size(classes_to_be_recompiled) > 0) {
+    	uncompiledClasses = '<br/><H1>WARNING these classes need to be recompiled in the org to allow proper Test Coverage to be calculated.</H1><br/><ul>\n';
+
+    	lo.forEach(classes_to_be_recompiled,function(value, key) {
+    		uncompiledClasses = uncompiledClasses + '\t<li>' + key + '</li>';
+    	});
+    	uncompiledClasses = uncompiledClasses + '</ul><br/>\n';
+    }
+
+    fs.writeFile(HTMLFilename, html + data + script_end + uncompiledClasses + html_end);
+    deferred.resolve();
+    return deferred.promise;
+}
+
+/**
+* Posts the data to coveralls
+*/
+// var postToCoveralls = function () {
+// 	'use strict';
+
+// 	var fs_stats, post_options,
+// 		deferred = Q.defer(),
+// 		coveralls_data = {
+// 			repo_token: process.env.COVERALLS_REPO_TOKEN,
+// 			service_name: 'travis-ci',
+// 			service_job_id: process.env.TRAVIS_JOB_ID,
+// 			source_files: lo.values(id_to_class_map)
+// 		};
+
+// 	console.log('Posting data to coveralls');
+
+// 	fs.writeFile('/tmp/coveralls_data.json', JSON.stringify(coveralls_data), function (fs_error) {
+// 		if (fs_error) {
+// 			deferred.reject(new Error(fs_error));
+// 		} else {
+// 			fs_stats = fs.statSync('/tmp/coveralls_data.json');
+
+// 			post_options = {
+// 				multipart: true,
+// 				data: {
+// 					json_file: restler.file('/tmp/coveralls_data.json', null, fs_stats.size, null, 'application/json')
+// 				}
+// 			};
+
+// 			restler.post('https://coveralls.io/api/v1/jobs', post_options).on("complete", function (data) {
+// 				deferred.resolve();
+// 			});
+// 		}
+// 	});
+
+// 	return deferred.promise;
+// };
+
+function queueSteps(res) {
+  Q.fcall(sfdcLogin)
+  	.then(buildClassIdToClassDataMap)
+  	.then(buildAddTriggersToClassIDMap)
+  	// .then(runAllTests)
+  	// .then(waitUntilTestsComplete)
+  	.then(buildCoverage)
+  	.then(saveToCSVCols)
+  	.then(writeHTML)
+  	.catch(function (error) {
+  		'use strict';
+  		console.log(error);
+  	})
+  	.done(function () {
+  		'use strict';
+  		sfdcLogout();
+      res.sendFile(path.join(__dirname + '/teststats.html'));
+  	});
+}
---
+++ node_modules/jsforce/test/analytics.test.js
@@ -0,0 +1,223 @@
+/*global describe, it, before, after */
+var TestEnv = require('./helper/testenv'),
+    assert = TestEnv.assert;
+
+var _      = require('lodash/core'),
+    sf     = require('../lib/jsforce'),
+    config = require('./config/salesforce');
+
+var testEnv = new TestEnv(config);
+
+/**
+ *
+ */
+describe("analytics", function() {
+
+  this.timeout(40000); // set timeout to 40 sec.
+
+  var conn = testEnv.createConnection();
+
+  var reportId;
+
+  /**
+   *
+   */
+  before(function(done) {
+    this.timeout(600000); // set timeout to 10 min.
+    testEnv.establishConnection(conn, done);
+  });
+
+  /**
+   *
+   */
+  before(function(done) {
+    conn.sobject('Report').findOne({ Name: 'Lead List Report'}, "Id").execute(function(err, res) {
+      if (err) { throw err; }
+      if (!res) { throw new Error("No Report Name 'Lead List Report' was found in the org."); }
+      reportId = res.Id;
+    }.check(done));
+  });
+
+  /**
+   *
+   */
+  describe("list recent reports", function() {
+    it("should return report infomation list", function(done) {
+      conn.analytics.reports(function(err, reports) {
+        if (err) { throw err; }
+        assert.ok(_.isArray(reports));
+        reports.forEach(function(report) {
+          assert.ok(_.isString(report.id));
+          assert.ok(_.isString(report.name));
+          assert.ok(_.isString(report.url));
+        });
+      }.check(done));
+    });
+  });
+
+  /**
+   *
+   */
+  describe("describe report", function() {
+    it("should return report metadata", function(done) {
+      conn.analytics.report(reportId).describe(function(err, meta) {
+        if (err) { throw err; }
+        assert.ok(_.isObject(meta));
+        assert.ok(_.isObject(meta.reportMetadata));
+        assert.ok(_.isObject(meta.reportTypeMetadata));
+        assert.ok(_.isObject(meta.reportExtendedMetadata));
+      }.check(done));
+    });
+  });
+
+  /**
+   *
+   */
+  describe("execute report synchronously", function() {
+    it("should return report execution result", function(done) {
+      conn.analytics.report(reportId).execute(function(err, result) {
+        if (err) { throw err; }
+        assert.ok(_.isObject(result));
+        assert.ok(result.hasDetailRows === false);
+        assert.ok(_.isObject(result.reportMetadata));
+        assert.ok(result.reportMetadata.id === reportId);
+        assert.ok(_.isObject(result.factMap));
+        assert.ok(_.isObject(result.factMap["T!T"]));
+        assert.ok(_.isUndefined(result.factMap["T!T"].rows));
+        assert.ok(_.isObject(result.factMap["T!T"].aggregates));
+      }.check(done));
+    });
+  });
+
+  /**
+   *
+   */
+  describe("execute report synchronously with details", function() {
+    it("should return report execution result", function(done) {
+      conn.analytics.report(reportId).execute({ details: true }, function(err, result) {
+        if (err) { throw err; }
+        assert.ok(_.isObject(result));
+        assert.ok(result.hasDetailRows === true);
+        assert.ok(_.isObject(result.reportMetadata));
+        assert.ok(result.reportMetadata.id === reportId);
+        assert.ok(_.isObject(result.factMap));
+        assert.ok(_.isObject(result.factMap["T!T"]));
+        assert.ok(_.isArray(result.factMap["T!T"].rows));
+        assert.ok(_.isObject(result.factMap["T!T"].aggregates));
+      }.check(done));
+    });
+  });
+
+  /**
+   *
+   */
+  describe("execute report synchronously with filters overrided", function() {
+    it("should return report execution result", function(done) {
+      var metadata = {
+        reportMetadata : {
+          historicalSnapshotDates : [],
+          reportFilters : [{
+            column: 'COMPANY',
+            operator: 'contains',
+            value: ',Inc.'
+          }]
+        }
+      };
+      conn.analytics.report(reportId).execute({ metadata : metadata, details: true }, function(err, result) {
+        if (err) { throw err; }
+        assert.ok(_.isObject(result));
+        assert.ok(_.isObject(result.reportMetadata));
+        assert.ok(_.isArray(result.reportMetadata.reportFilters));
+        assert.ok(result.reportMetadata.id === reportId);
+        assert.ok(_.isObject(result.factMap));
+        assert.ok(_.isObject(result.factMap["T!T"]));
+        assert.ok(_.isArray(result.factMap["T!T"].rows));
+        assert.ok(_.isObject(result.factMap["T!T"].aggregates));
+      }.check(done));
+    });
+  });
+
+  var instanceId;
+
+  /**
+   *
+   */
+  describe("execute report asynchronously", function() {
+    it("should return report instance info", function(done) {
+      conn.analytics.report(reportId).executeAsync(function(err, instance) {
+        if (err) { throw err; }
+        assert.ok(_.isObject(instance));
+        assert.ok(_.isString(instance.id));
+        assert.ok(_.isString(instance.status));
+        assert.ok(_.isString(instance.requestDate));
+        instanceId = instance.id;
+      }.check(done));
+    });
+  });
+
+  /**
+   *
+   */
+  describe("list asynchronously executed report instances", function() {
+    it("should return report instance list", function(done) {
+      var rinstance = conn.analytics.report(reportId).instances(function(err, instances) {
+        if (err) { throw err; }
+        assert.ok(_.isArray(instances));
+        instances.forEach(function(instance) {
+          assert.ok(_.isString(instance.id));
+          assert.ok(_.isString(instance.status));
+          assert.ok(_.isString(instance.requestDate));
+        });
+      }.check(done));
+    });
+  });
+
+  /**
+   *
+   */
+  describe("retrieve asynchronously executed report result", function() {
+    it("should return report execution result", function(done) {
+      conn.analytics.report(reportId).instance(instanceId).retrieve(function(err, result) {
+        if (err) { throw err; }
+        assert.ok(_.isObject(result));
+        assert.ok(_.isObject(result.attributes));
+        assert.ok(result.attributes.id === instanceId);
+        assert.ok(_.isString(result.attributes.status));
+        assert.ok(_.isString(result.attributes.requestDate));
+        if (result.attributes.status == 'Success') {
+          assert.ok(_.isObject(result.reportMetadata));
+          assert.ok(result.reportMetadata.id === reportId);
+        }
+      }.check(done));
+    });
+  });
+
+  /**
+   *
+   */
+  describe("explain query plan of report", function() {
+    it("should get explain result", function(done) {
+      conn.analytics.report(reportId).explain(function(err, result) {
+        if (err) { throw err; }
+        assert.ok(_.isArray(result.plans));
+        for (var i=0; i<result.plans.length; i++) {
+          var plan = result.plans[i];
+          assert.ok(_.isNumber(plan.cardinality));
+          assert.ok(_.isArray(plan.fields));
+          assert.ok(_.isString(plan.leadingOperationType));
+          assert.ok(_.isNumber(plan.relativeCost));
+          assert.ok(_.isNumber(plan.sobjectCardinality));
+          assert.ok(_.isString(plan.sobjectType));
+        }
+      }.check(done));
+    });
+  });
+
+  /**
+   *
+   */
+  after(function(done) {
+    testEnv.closeConnection(conn, done);
+  });
+
+});
